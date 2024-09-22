6ad0286f1e7a3e2a81ef09860621bbc090ecd16b
enzy-o0 <oej3659@gmail.com>
2024-09-22 22:17:25 +0900

    ADD 개시하기 모달 패러럴, 인터셉트 처리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,10 @@ type Props = Readonly<{
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
+      <head>
+        <meta name="color-scheme" content="light only" />
+        <meta name="supported-color-schemes" content="light" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
