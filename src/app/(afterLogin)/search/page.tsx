26d9cb4a6a5b53bda3d0d16206cfe3c48c842b33
enzy-o0 <oej3659@gmail.com>
2024-09-22 23:17:35 +0900

    ADD search 페이지 구현

---
+++ src/app/(afterLogin)/search/page.tsx
@@ -0,0 +1,41 @@
+import BackButton from "../_component/BackButton";
+import Post from "../_component/Post";
+import SearchForm from "../_component/SearchForm";
+import Tab from "./_component/Tab";
+import style from "./search.module.scss";
+
+type SearchProps = {
+  searchParams: { q: string; f?: string; pf?: string };
+};
+
+export default function Search({ searchParams }: SearchProps) {
+  return (
+    <main className={style.main}>
+      <div className={style.searchTop}>
+        <div className={style.searchZone}>
+          <div className={style.buttonZone}>
+            <BackButton />
+          </div>
+          <div className={style.formZone}>
+            <SearchForm q={searchParams.q} />
+          </div>
+        </div>
+        <Tab />
+      </div>
+      <div className={style.list}>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        {/*<SearchResult searchParams={searchParams} />*/}
+      </div>
+    </main>
+  );
+}
