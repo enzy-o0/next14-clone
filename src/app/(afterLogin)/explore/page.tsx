7ee44c92fe2c053e7183b6fdf2bee304ccafa5c9
enzy-o0 <oej3659@gmail.com>
2024-09-22 22:19:08 +0900

    ADD explore 페이지 구현 및 RightSearchZone, SearchForm, TrendSection 컴포넌트 분리

--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,5 +1,23 @@
-import styles from "@/app/(afterLogin)/layout.module.scss";
+import style from "./explore.module.scss";
+import SearchForm from "../_component/SearchForm";
+import Trend from "../_component/Trend";
 
 export default function Explore() {
-  return <main className={styles.main}>검색 페이지</main>;
+  return (
+    <main className={style.main}>
+      <div className={style.formZone}>
+        <SearchForm />
+      </div>
+      <div className={style.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </main>
+  );
 }
