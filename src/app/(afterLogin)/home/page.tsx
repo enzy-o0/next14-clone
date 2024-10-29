cc6bf1317ec41a0305452ddf36948d451915ecd3
enzy-o0 <oej3659@gmail.com>
2024-10-29 23:21:24 +0900

    ADD tanstack query 설치, provider 설정

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,20 +1,58 @@
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
 import Post from "../_component/Post";
 import PostForm from "./_component/PostForm";
 import Tab from "./_component/Tab";
 import TabProvider from "./_component/TabProvider";
 import style from "./home.module.scss";
+import { revalidatePath, revalidateTag } from "next/cache";
 
-const Home = () => {
+async function getPostRecommends() {
+  const res = await fetch("http://localhost:9090/api/postRecommend", {
+    next: {
+      tags: ["posts", "recommends"],
+    },
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  // 캐시 초기화
+  // 태그
+  // revalidateTag("recommends")
+
+  // 페이지 전체 새로고침
+  // revalidatePath("/home")
+
+  return res.json();
+}
+
+export default async function Home() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["posts", "recommends"],
+    queryFn: getPostRecommends,
+  });
+
+  const dehydratedState = dehydrate(queryClient);
+
+  // queryClient.getQueryData(["posts", "recommends"]);
+
   return (
     <main className={style.main}>
-      <TabProvider>
-        <Tab />
-        <PostForm />
-        <Post />
-        <Post /> <Post /> <Post /> <Post /> <Post /> <Post />
-      </TabProvider>
+      <HydrationBoundary state={dehydratedState}>
+        <TabProvider>
+          <Tab />
+          <PostForm />
+          <Post />
+          <Post /> <Post /> <Post /> <Post /> <Post /> <Post />
+        </TabProvider>
+      </HydrationBoundary>
     </main>
   );
-};
-
-export default Home;
+}
